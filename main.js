78f7f36b8653b6a3d5bb6c5b3049a046549365a2
EmilVattai <vattai.emil@gmail.com>
2024-11-27 20:12:04 +0100

    Game majdnem kész.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
 const prompt = require('prompt-sync')();
 let isGameRun = true;
-const inHor = ['A', 'B', 'C'];
-const inVert = ['1', '2', '3'];
+const inVert = ['A', 'B', 'C'];
+const inHor = ['1', '2', '3'];
 let errorMessage = '';
+let actualPlayer = '';
+let humanPlayers = 0;
+
+let playerInfo = [
+   {
+      character: ' ',
+      isHumanoid: false
+   },
+   {
+      character: ' ',
+      isHumanoid: false
+   }
+];
+
+let isWin = false;
+let winnerPlayer = '';
+
 let gameField = [
    [ ' ',' ',' '],
    [ ' ',' ',' '],
@@ -18,7 +35,58 @@ function printGameField() {
    console.log();
 }
 
+function isGameWin() {
+   if (gameField[0][0] === gameField[0][1] && gameField[0][1] === gameField[0][2] && gameField[0][0] !== ' ') {
+      winnerPlayer = gameField[0][0];
+      return true;
+   }
+   if (gameField[1][0] === gameField[1][1] && gameField[1][1] === gameField[1][2] && gameField[1][0] !== ' ') {
+      winnerPlayer = gameField[1][0];
+      return true;
+   }
+   if (gameField[2][0] === gameField[2][1] && gameField[2][1] === gameField[2][2] && gameField[2][0] !== ' ') {
+      winnerPlayer = gameField[2][0];
+      return true;
+   }
 
+   if (gameField[0][0] === gameField[1][0] && gameField[1][0] === gameField[2][0] && gameField[0][0] !== ' ') {
+      winnerPlayer = gameField[0][0];
+      return true;
+   }
+   if (gameField[0][1] === gameField[1][1] && gameField[1][1] === gameField[2][1] && gameField[0][1] !== ' ') {
+      winnerPlayer = gameField[0][1];
+      return true;
+   }
+   if (gameField[0][2] === gameField[1][2] && gameField[1][2] === gameField[2][2] && gameField[0][2] !== ' ') {
+      winnerPlayer = gameField[0][2];
+      return true;
+   }
+
+   if (gameField[0][0] === gameField[1][1] && gameField[1][1] === gameField[2][2] && gameField[0][0] !== ' ') {
+      winnerPlayer = gameField[0][0];
+      return true;
+   }
+   if (gameField[0][2] === gameField[1][1] && gameField[1][1] === gameField[2][0] && gameField[0][2] !== ' ') {
+      winnerPlayer = gameField[0][2];
+      return true;
+   }
+}
+
+function printError(errorMsg) {
+   if (errorMessage !== '') {
+      console.log('Hiba: ' + errorMsg);
+   } else {
+      console.log();
+   }
+}
+
+function isActualPlayerHum(actualChar) {
+   if (playerInfo[0].character === actualChar) {
+      return playerInfo[0].isHumanoid;
+   } else if (playerInfo[1].character === actualChar) {
+      return playerInfo[1].isHumanoid;
+   }
+}
 
 /*
       A   B   C
@@ -28,42 +96,87 @@ function printGameField() {
  3    O | O | O 
 
    gameField[0][0]
+   [Horizontal][Vertical]
 */
+
+let gameNotRun = true;
+while(gameNotRun) {
+   const inpPlayerNum = prompt('Játékosok száma [1-2]: ');
+   if (inpPlayerNum !== '1' && inpPlayerNum !== '2') {
+      console.log('Baromarcú!!! 1 vagy 2?????');
+   } else {
+      if (inpPlayerNum === '1') {
+         playerInfo[0].isHumanoid = true;
+      }
+      else {
+         playerInfo[0].isHumanoid = true;
+         playerInfo[1].isHumanoid = true;
+      }
+      gameNotRun = false;
+   }
+}
+
+
+let randomNum = Math.floor(Math.random() * 10) + 1;
+if(randomNum % 2) {
+   playerInfo[0].character = 'X';
+   playerInfo[1].character = 'O';
+   actualPlayer = 'X';
+} else {
+   playerInfo[0].character = 'O';
+   playerInfo[1].character = 'X';
+   actualPlayer = 'O';
+}
+
 
 while (isGameRun) {
    console.clear();
    printGameField();
    console.log();
-   if (errorMessage !== '') {
-      console.log('Hiba: ' + errorMessage);
-   } else {
-      console.log();
-   }
+   printError('');
    console.log();
-   const input = prompt('Kérek egy mezőt: ');
-   if (inHor.includes(input[0]) & inVert.includes(input[1])) {
-      const Hor = inHor.indexOf(input[0]);
-      const Vert = inVert.indexOf(input[1]);
-      if(gameField[Hor][Vert] === ' ') {
-         gameField[Hor][Vert] = 'X';
-         errorMessage = '';
+
+   if(isActualPlayerHum(actualPlayer)) {
+      const input = prompt('Kérek egy mezőt [' + actualPlayer + ']:');
+      if (inVert.includes(input[0]) && inHor.includes(input[1])) {
+         const Hor = inHor.indexOf(input[1]);
+         const Vert = inVert.indexOf(input[0]);
+         if(gameField[Hor][Vert] === ' ') {
+            gameField[Hor][Vert] = actualPlayer;
+            actualPlayer = actualPlayer === 'X' ? 'O' : 'X';
+            errorMessage = '';
+         } else {
+            errorMessage = 'Ez a mező már foglalt!';
+         }    
       } else {
-         errorMessage = 'Ez a mező már foglalt!';
+         if (input === 'XX') {
+            isGameRun = false;
+         } else {
+            errorMessage = 'Helytelen mező!';
+         }
       }
-      
+   } else {
+      // AI
+
+
+
+
    }
-   else {
-      if (input === 'XX')
-      {
-         isGameRun = false;
-      }
+
+   isWin = isGameWin();  
+   if(isWin) {
+      isGameRun = false;
    }
-
-
-
-
-
-   
 }
 
+
+
+if (isWin) {
+   console.clear();
+   printGameField();
+   console.log();
+   printError('');
+   console.log();
+   console.log('Gratulálunk ' + winnerPlayer + '! Nyertél.');
+}
 
